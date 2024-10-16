fc39403ac4f2f735922b1d30175dc3bd8b4f3f08
Farhan Syah <bizimpulse@gmail.com>
2024-10-16 11:00:12 +0800

    Sample testing

---
+++ 3. Project/testing/load-testing.js
@@ -0,0 +1,38 @@
+import { check } from "k6";
+import exec from "k6/execution";
+import http from "k6/http";
+import { Counter } from "k6/metrics";
+
+export const options = {
+  scenarios: {
+    // ramping: {
+    //   executor: "ramping-vus",
+    //   startVUs: 0,
+    //   stages: [{ duration: "10s", target: 20000 }],
+    //   gracefulStop: "5s",
+    // },
+    shared: {
+      executor: "shared-iterations",
+      vus: 100,
+      iterations: 1500000,
+      maxDuration: "10s",
+      gracefulStop: "5s",
+    },
+  },
+};
+const myCounter = new Counter("my_counter", false);
+
+export default function () {
+  let res = http.get("http://localhost:3000");
+
+  let checkStatus = check(res, {
+    "status code MUST be 200": (res) => res.status == 200,
+  });
+
+  if (!checkStatus) {
+    exec.test.abort();
+  }
+  // let ipAddress = JSON.parse(res.body).ipAddress;
+  // console.log(ipAddress);
+  // myCounter.add(1, { ipAddress: ipAddress });
+}
